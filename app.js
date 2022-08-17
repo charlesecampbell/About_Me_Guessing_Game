e8f027c8dee89c9cc44f9dc86a7859bfd555b61b
Charles E. Campbell <wbsbp88@gmail.com>
2022-08-16 22:19:13 -0400

    Guessing Game

---
+++ app.js
@@ -0,0 +1,70 @@
+'use strict';
+let name = prompt('What is your name?');
+alert("Your name is : " + Name );
+user = user.toUpperCase() || user.toLowerCase();
+
+// 1. If conditional based confirm command
+let user = prompt('Are you over 21?');
+user = user.toUpperCase() || user.toLowerCase();
+// one conditions have to be true
+if (user === 'yes'|| user === 'y') {
+//   console.log('Yes, you are old enough!');
+  alert('Yes, you are old enough!');
+} // only one of these have to be true
+else if (user === 'no' || user === 'n') {
+//   console.log('Maybe when you are older!');
+  alert('Maybe when you are older!');
+} // only no can be false
+
+// 2. If conditional based confirm command
+let user = prompt('Can you drive a car?');
+user = user.toUpperCase() || user.toLowerCase();
+// one conditions have to be true
+if (user === 'yes'|| user === 'y') {
+    //   console.log('Yes, I have my license!');
+      alert('Yes, I have my license!');
+    } // only one of these have to be true
+    else if (user === 'no' || user === 'n') {
+    //   console.log('No! I do not drive!');
+      alert('No I do not drive!');
+    } // only no can be false
+
+// 3. If conditional based confirm command
+    let user = prompt('Will you skydive with me?');
+    user = user.toUpperCase() || user.toLowerCase();
+// one conditions have to be true
+if (user === 'yes'|| user === 'y') {
+    //   console.log('Yes, I would love to!');
+      alert('Yes, I would love to!');
+    } // only one of these have to be true
+    else if (user === 'no' || user === 'n') {
+    //   console.log('No! I am terrified of heights!');
+      alert('No! I am terrified of heights!');
+    } // only no can be false
+
+// 4. If conditional based confirm command
+let user = prompt('Can you dance?');
+user = user.toUpperCase() || user.toLowerCase();
+    // one conditions have to be true
+if (user === 'yes'|| user === 'y') {
+    //   console.log('Yes, I am a very good dancer!');
+       alert('Yes, I am a very good dancer!');
+    } // only one of these have to be true
+    else if (user === 'no' || user === 'n') {
+    //   console.log('No! I have two left feet!');
+        alert('No! I have two left feet!');
+    } // only no can be false
+
+// 5. If conditional based confirm command
+let user = prompt('Do you play video games?');
+user = user.toUpperCase() || user.toLowerCase();
+    // one conditions have to be true
+if (user === 'yes'|| user === 'y') {
+    //   console.log('Yes, I am a big time gamer!');
+       alert('Yes, I am a a big time gamer!');
+    } // only one of these have to be true
+    else if (user === 'no' || user === 'n') {
+    //   console.log('No! I hate video games!');
+        alert('No! I hate video games!');
+    } // only no can be false
+
