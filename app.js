26089f05200e41ece8fdfde8dd6390cc87b750a3
Charles E. Campbell <wbsbp88@gmail.com>
2022-08-17 22:38:49 -0400

    Reading Note Class 04

--- app.js
+++ app.js
@@ -1,85 +1,98 @@
-'use strict';
+'use strict';//strict syntax
+
+
+
 let name = prompt('What is your name?');
 alert('Hello ' + Name );
 user = user.toLowerCase();
-document.write('Greeting ' + Name + ', Answer a few yes or no questions about me');
+//console.log(Name);
+document.write('Greetings ' + Name + ', Answer a few yes or no questions about me');
 
-//Questions
-// 1. If conditional based confirm command
+//Ask Several Questions
+// Question 1. 
 let user = prompt('Am I over 21 yeas old?');
-if (typeof (user) === 'string'){
+if (typeof (user) === 'string') {
 user = user.toLowerCase();
 }
-// one conditions have to be true
-if (user === 'yes'|| user === 'y') {
-//   console.log('Correct, I am over 21!');
-//In case yes, correct
-switch (user) {
-  case 'yes':
-  alert('Correct, I am over 21!');
-// only one of these have to be true
-  break;
+//If no is Incorrect
+if (user === 'no') {
+alert('Incorrect! I am over 21');
+//console.log('Correct, I am over 21!');
+} else if (user === 'yes') {
+  //   console.log('Correct answer!');
+  alert('Correct answer!');
+}
 
-// only no can be false
+// Question 2.
+let degree = prompt('Do I have a college degree?');
+if (typeof (degree) === 'string') {
+degree = degree.toLowerCase();
+}
+switch (degree) {
+  case 'yes':
+  alert('Correct, I have a Bachelor\'s Degree!');
+  break;
   case 'no':
   alert('Incorrect answer!');
 //   console.log('Incorrect answer!');
 break;
 } 
 
-// 2. If conditional based confirm command
-let degree = prompt('Do I have a college degree?');
-if (typeof (degree) === 'string'){
-degree = user.toLowerCase();
-}
-// one conditions have to be true
-if (degree === 'yes'|| degree === 'y') {
-    //   console.log('Correct, I have a Bachelor\'s Degree!');
-      alert('Correct, I have a Bachelor\'s Degree!');
-    } // only one of these have to be true
-    else if (degree === 'no' || degree === 'n') {
-    //   console.log('Incorrect!');
-      alert('Incorrect!');
-    } // only no can be false
-
-// 3. If conditional based confirm command
+// Question 3.
     let social = prompt('Have I been a Social Worker?');
     if (typeof (social === 'string')){
       social = user.toLowerCase();
     } // one conditions have to be true
 if (social === 'yes'|| social === 'y') {
-    //   console.log('Correct, I have been a Social Worker!');
-      alert('Correct, I have been a Social Worker!');
-    } // only one of these have to be true
-    else if (social === 'no' || social === 'n') {
-    //   console.log('Incorrect Answer!');
-      alert('Incorrect Answer');
-    } // only no can be false
 
-// 4. If conditional based confirm command
+//In case yes, correct
+switch (social) {
+  case 'yes':
+  alert('Correct, I have a Bachelor\'s Degree!');
+  break;
+
+  case 'no':
+  alert('Incorrect answer!');
+//   console.log('Incorrect answer!');
+break;
+} 
+}
+
+// Question 4.
 let dancer = prompt('Can I dance?');
 if (typeof (dancer) === 'string'){
 dancer = dancer.toLowerCase();
 }// one conditions have to be true
 if (dancer === 'yes'|| dancer === 'y') {
     //   console.log('Correct, I am a very good dancer!');
-       alert('Correct, I am a very good dancer!');
-    } // only one of these have to be true
+    } 
     else if (dancer === 'no' || dancer === 'n') {
-    //   console.log('Incorrect Answer');
-        alert('Incorrect Answer');
-    } // only no can be false
+//In case yes, correct
+switch (dancer) {
+  case 'yes':
+  alert('Correct, I have a Bachelor\'s Degree!');
+  break;
 
-// 5. If conditional based confirm command
+  case 'no':
+  alert('Incorrect answer!');
+//   console.log('Incorrect answer!');
+break;
+} 
+}
+
+// Question 5.
 let startup = prompt('Do I own a tech startup?');
 if (typeof (startup) === 'string'){
 startup = startup.toLowerCase();
-}// one conditions have to be true
-if (startup === 'yes'|| startup === 'y') {
-    //   console.log('Correct, I own Cuptoopia!!');
-       alert('Correct, I own Cuptoopia!');
-    } // only one of these have to be true
-    else if (startup === 'no' || startup === 'n') {
-    //   console.log('Incorrect');
-        alert('Incorrect!');
-    } // only no can be false
+}   
+    //In case yes, correct
+switch (startup) {
+  case 'yes':
+  alert('Correct, I own Cuptoopia!!');
+  break;
+
+  case 'no':
+  alert('Incorrect answer!');
+//   console.log('Incorrect answer!');
+break;
+} 
