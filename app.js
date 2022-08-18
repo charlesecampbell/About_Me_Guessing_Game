614aeed56aefc72cdb7dab9a3c5a5a4bee0c0930
Charles E. Campbell <wbsbp88@gmail.com>
2022-08-17 20:47:27 -0400

    updates

--- app.js
+++ app.js
@@ -1,70 +1,85 @@
 'use strict';
 let name = prompt('What is your name?');
-alert("Your name is : " + Name );
-user = user.toUpperCase() || user.toLowerCase();
+alert('Hello ' + Name );
+user = user.toLowerCase();
+document.write('Greeting ' + Name + ', Answer a few yes or no questions about me');
 
+//Questions
 // 1. If conditional based confirm command
-let user = prompt('Are you over 21?');
-user = user.toUpperCase() || user.toLowerCase();
+let user = prompt('Am I over 21 yeas old?');
+if (typeof (user) === 'string'){
+user = user.toLowerCase();
+}
 // one conditions have to be true
 if (user === 'yes'|| user === 'y') {
-//   console.log('Yes, you are old enough!');
-  alert('Yes, you are old enough!');
-} // only one of these have to be true
-else if (user === 'no' || user === 'n') {
-//   console.log('Maybe when you are older!');
-  alert('Maybe when you are older!');
-} // only no can be false
+//   console.log('Correct, I am over 21!');
+//In case yes, correct
+switch (user) {
+  case 'yes':
+  alert('Correct, I am over 21!');
+// only one of these have to be true
+  break;
+
+// only no can be false
+  case 'no':
+  alert('Incorrect answer!');
+//   console.log('Incorrect answer!');
+break;
+} 
 
 // 2. If conditional based confirm command
-let user = prompt('Can you drive a car?');
-user = user.toUpperCase() || user.toLowerCase();
+let degree = prompt('Do I have a college degree?');
+if (typeof (degree) === 'string'){
+degree = user.toLowerCase();
+}
 // one conditions have to be true
-if (user === 'yes'|| user === 'y') {
-    //   console.log('Yes, I have my license!');
-      alert('Yes, I have my license!');
+if (degree === 'yes'|| degree === 'y') {
+    //   console.log('Correct, I have a Bachelor\'s Degree!');
+      alert('Correct, I have a Bachelor\'s Degree!');
     } // only one of these have to be true
-    else if (user === 'no' || user === 'n') {
-    //   console.log('No! I do not drive!');
-      alert('No I do not drive!');
+    else if (degree === 'no' || degree === 'n') {
+    //   console.log('Incorrect!');
+      alert('Incorrect!');
     } // only no can be false
 
 // 3. If conditional based confirm command
-    let user = prompt('Will you skydive with me?');
-    user = user.toUpperCase() || user.toLowerCase();
-// one conditions have to be true
-if (user === 'yes'|| user === 'y') {
-    //   console.log('Yes, I would love to!');
-      alert('Yes, I would love to!');
+    let social = prompt('Have I been a Social Worker?');
+    if (typeof (social === 'string')){
+      social = user.toLowerCase();
+    } // one conditions have to be true
+if (social === 'yes'|| social === 'y') {
+    //   console.log('Correct, I have been a Social Worker!');
+      alert('Correct, I have been a Social Worker!');
     } // only one of these have to be true
-    else if (user === 'no' || user === 'n') {
-    //   console.log('No! I am terrified of heights!');
-      alert('No! I am terrified of heights!');
+    else if (social === 'no' || social === 'n') {
+    //   console.log('Incorrect Answer!');
+      alert('Incorrect Answer');
     } // only no can be false
 
 // 4. If conditional based confirm command
-let user = prompt('Can you dance?');
-user = user.toUpperCase() || user.toLowerCase();
-    // one conditions have to be true
-if (user === 'yes'|| user === 'y') {
-    //   console.log('Yes, I am a very good dancer!');
-       alert('Yes, I am a very good dancer!');
+let dancer = prompt('Can I dance?');
+if (typeof (dancer) === 'string'){
+dancer = dancer.toLowerCase();
+}// one conditions have to be true
+if (dancer === 'yes'|| dancer === 'y') {
+    //   console.log('Correct, I am a very good dancer!');
+       alert('Correct, I am a very good dancer!');
     } // only one of these have to be true
-    else if (user === 'no' || user === 'n') {
-    //   console.log('No! I have two left feet!');
-        alert('No! I have two left feet!');
+    else if (dancer === 'no' || dancer === 'n') {
+    //   console.log('Incorrect Answer');
+        alert('Incorrect Answer');
     } // only no can be false
 
 // 5. If conditional based confirm command
-let user = prompt('Do you play video games?');
-user = user.toUpperCase() || user.toLowerCase();
-    // one conditions have to be true
-if (user === 'yes'|| user === 'y') {
-    //   console.log('Yes, I am a big time gamer!');
-       alert('Yes, I am a a big time gamer!');
+let startup = prompt('Do I own a tech startup?');
+if (typeof (startup) === 'string'){
+startup = startup.toLowerCase();
+}// one conditions have to be true
+if (startup === 'yes'|| startup === 'y') {
+    //   console.log('Correct, I own Cuptoopia!!');
+       alert('Correct, I own Cuptoopia!');
     } // only one of these have to be true
-    else if (user === 'no' || user === 'n') {
-    //   console.log('No! I hate video games!');
-        alert('No! I hate video games!');
+    else if (startup === 'no' || startup === 'n') {
+    //   console.log('Incorrect');
+        alert('Incorrect!');
     } // only no can be false
-
